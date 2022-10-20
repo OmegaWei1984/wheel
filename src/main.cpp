00fac7f26d82a31b74ed4010fc044d7c304394a0
OmegaWei1984 <omegawei1984@gmail.com>
2022-10-20 23:28:00 +0800

    test: 启动 service

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "Wheel.h"
 
+int test()
+{
+    auto pingType = make_shared<string>("ping");
+    uint32_t ping1 = Wheel::inst->newService(pingType);
+    uint32_t ping2 = Wheel::inst->newService(pingType);
+    uint32_t pong = Wheel::inst->newService(pingType);
+    return 0;
+}
+
 int main(void) {
     new Wheel();
     Wheel::inst->start();
+    test();
     Wheel::inst->wait();
     return 0;
 }
