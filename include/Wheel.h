bf89be20e552a258a13010aa25b7e1df0d0946be
OmegaWei1984 <omegawei1984@gmail.com>
2022-10-24 00:17:25 +0800

    feat: 全局消息队列 pop 和 push

--- include/Wheel.h
+++ include/Wheel.h
@@ -10,22 +10,28 @@
 class Wheel
 {
 public:
-    static Wheel *inst;
-    unordered_map<uint32_t, shared_ptr<Service>> services;
-    uint32_t maxId = 0;
-    shared_mutex rwlock;
+	static Wheel* inst;
+	unordered_map<uint32_t, shared_ptr<Service>> services;
+	uint32_t maxId = 0;
+	shared_mutex rwlock;
 
-    Wheel();
-    void start();
-    void wait();
-    uint32_t newService(shared_ptr<string> type);
-    void killService(uint32_t id);
+	Wheel();
+	void start();
+	void wait();
+	uint32_t newService(shared_ptr<string> type);
+	void killService(uint32_t id);
+	void send(uint32_t toId, shared_ptr<BaseMsg>);
+	shared_ptr<Service> popGQueue();
+	void pushGQueue(shared_ptr<Service> srv);
 
 private:
-    int WORKER_NUM = 3;
-    vector<Worker *> workers;
-    vector<thread *> workerThreads;
+	int WORKER_NUM = 3;
+	vector<Worker*> workers;
+	vector<thread*> workerThreads;
+	queue<shared_ptr<Service>> gQueue;
+	int gQueueLen = 0;
+	mutex gQueueMutex;
 
-    void startWorker();
-    shared_ptr<Service> getService(uint32_t id);
+	void startWorker();
+	shared_ptr<Service> getService(uint32_t id);
 };
--- src/Wheel.cpp
+++ src/Wheel.cpp
@@ -3,81 +3,108 @@
 
 using namespace std;
 
-Wheel *Wheel::inst;
+Wheel* Wheel::inst;
 
 Wheel::Wheel()
 {
-    inst = this;
+	inst = this;
 }
 
 void Wheel::startWorker()
 {
-    for (int i = 0; i < WORKER_NUM; ++i)
-    {
-        cout << "start worker thread: " << i << endl;
-        Worker *worker = new Worker();
-        worker->id = i;
-        worker->eachNum = 2 << i;
-        thread *wt = new thread(*worker);
-        workers.push_back(worker);
-        workerThreads.push_back(wt);
-    }
+	for (int i = 0; i < WORKER_NUM; ++i)
+	{
+		cout << "start worker thread: " << i << endl;
+		Worker* worker = new Worker();
+		worker->id = i;
+		worker->eachNum = 2 << i;
+		thread* wt = new thread(*worker);
+		workers.push_back(worker);
+		workerThreads.push_back(wt);
+	}
 }
 
 void Wheel::start()
 {
-    cout << "hello, wheel!" << endl;
-    startWorker();
+	cout << "hello, wheel!" << endl;
+	startWorker();
 }
 
 void Wheel::wait()
 {
-    if (workerThreads[0])
-    {
-        workerThreads[0]->join();
-    }
+	if (workerThreads[0])
+	{
+		workerThreads[0]->join();
+	}
 }
 
 uint32_t Wheel::newService(shared_ptr<string> type)
 {
-    auto srv = make_shared<Service>();
-    srv->type = type;
-    {
-        unique_lock<shared_mutex> wlock(rwlock);
-        srv->id = maxId;
-        ++maxId;
-        services.emplace(srv->id, srv);
-    }
-    srv->onInit();
-    return srv->id;
+	auto srv = make_shared<Service>();
+	srv->type = type;
+	{
+		unique_lock<shared_mutex> wlock(rwlock);
+		srv->id = maxId;
+		++maxId;
+		services.emplace(srv->id, srv);
+	}
+	srv->onInit();
+	return srv->id;
 }
 
 shared_ptr<Service> Wheel::getService(uint32_t id)
 {
-    shared_ptr<Service> srv = NULL;
-    {
-        shared_lock<shared_mutex> rlock(rwlock);
-        unordered_map<uint32_t, shared_ptr<Service>>::
-            iterator iter = services.find(id);
-        if (iter != services.end())
-        {
-            srv = iter->second;
-        }
-    }
-    return srv;
+	shared_ptr<Service> srv = NULL;
+	{
+		shared_lock<shared_mutex> rlock(rwlock);
+		unordered_map<uint32_t, shared_ptr<Service>>::
+			iterator iter = services.find(id);
+		if (iter != services.end())
+		{
+			srv = iter->second;
+		}
+	}
+	return srv;
 }
 
 void Wheel::killService(uint32_t id)
 {
-    shared_ptr<Service> srv = getService(id);
-    if (!srv)
-    {
-        return;
-    }
-    srv->onExit();
-    srv->isExiting = true;
-    {
-        unique_lock<shared_mutex> wlock(rwlock);
-        services.erase(id);
-    }
+	shared_ptr<Service> srv = getService(id);
+	if (!srv)
+	{
+		return;
+	}
+	srv->onExit();
+	srv->isExiting = true;
+	{
+		unique_lock<shared_mutex> wlock(rwlock);
+		services.erase(id);
+	}
+}
+
+void Wheel::send(uint32_t toId, shared_ptr<BaseMsg> msg)
+{
+
+}
+
+shared_ptr<Service> Wheel::popGQueue()
+{
+	shared_ptr<Service> srv = NULL;
+	{
+		const lock_guard<mutex> gQueueLock(gQueueMutex);
+		if (!gQueue.empty())
+		{
+			srv = gQueue.front();
+			gQueue.pop();
+			--gQueueLen;
+		}
+		return srv;
+	}
+}
+
+void Wheel::pushGQueue(shared_ptr<Service> srv)
+{
+	const lock_guard<mutex> gQueueLock(gQueueMutex);
+	gQueue.push(srv);
+	++gQueueLen;
 }
